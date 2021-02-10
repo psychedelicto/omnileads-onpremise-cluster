eb942dee184aa647d305b8f7d1bd5fcb1f2554ac
fpignataro <>
2021-02-10 18:49:52 -0300

    install docker

--- user_data/redis.sh
+++ user_data/redis.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
+OMLCOMP=redis
 RELEASE=main
 OMLDIR=/opt/omnileads/
 INSTALL_DOCKER=TRUE
 
+cd /var/tmp
 git clone https://github.com/psychedelicto/omnileads-onpremise-cluster.git
+cd omnileads-onpremise-cluster
 git checkout $RELEASE
-mkdir -p $OMLDIR
+
+mkdir -p $OMLDIR/$OMLCOMP
 cp files/docker-compose.yml $OMLDIR
 cp files/redis-service /etc/systemd/system
 
 if ["$INSTALL_DOCKER" == "TRUE"]
 then
-chmod +x ./files/install_docker.sh 
+chmod +x ./files/install_docker.sh
 ./files/install_docker.sh
 fi
 
 cd $OMLDIR/redis
 docker-compose up -d
-
-reboot
