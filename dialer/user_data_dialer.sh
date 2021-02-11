8115bffd9d247bd44cc3803cbb5c1cc4bcc756e4
fpignataro <>
2021-02-11 10:41:12 -0300

    separacion componentes

---
+++ dialer/user_data_dialer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+OMLCOMP=dialer
+RELEASE=main
+OMLDIR=/opt/omnileads/
+
+mysql_host=localhost
+mysql_database=wombat
+mysql_username=wombat
+mysql_password=admin123
+
+yum install -y git
+
+cd /var/tmp
+git clone https://github.com/psychedelicto/omnileads-onpremise-cluster.git
+cd omnileads-onpremise-cluster
+git checkout $RELEASE
+
+if [[ "$mysql_host" == "localhost" ]]
+then
+chmod +x ./mariadb/mariadb.sh
+sh ./mariadb/mariadb.sh $mysql_username $mysql_password
+fi
+cd $OMLCOMP
+sh dialer.sh $mysql_host $mysql_database $mysql_username $mysql_password
+
+reboot
---
+++ mariadb/mariadb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mysql_username=$1
+mysql_password=$2
+
+echo "******************** prereq selinux and firewalld ***************************"
+echo "******************** prereq selinux and firewalld ***************************"
+systemctl disable firewalld
+
+echo "******************** yum install mariaDB ***************************"
+echo "******************** yum install mariaDB ***************************"
+yum install mariadb-server -y
+systemctl start mariadb
+systemctl enable mariadb
+
+echo "******************** postinstall configuration ***************************"
+echo "******************** postinstall configuration ***************************"
+mysql -e "GRANT ALL ON *.* to '$mysql_username'@'%' IDENTIFIED BY '$mysql_password' WITH GRANT OPTION;"
+
+reboot
