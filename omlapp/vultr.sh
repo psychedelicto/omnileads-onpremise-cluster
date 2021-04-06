37b88c82025f9f37a17b8d86dd06ac299ab7f056
fpignataro <>
2021-04-06 12:04:47 -0300

    clouds

--- omlapp/vultr.sh
+++ omlapp/vultr.sh
@@ -4,11 +4,12 @@ REPO_URL=https://github.com/psychedelicto/omnileads-onpremise-cluster.git
 REPO_RELEASE=develop
 
 ############### VULRT and OMNILEADS env settings #############################
-echo "******************** fix hostname and localhost issue on some cloud instances ***************************"
-echo "******************** fix hostname and localhost issue on some cloud instances ***************************"
 TEMP_HOSTNAME=$(hostname)
 sed -i 's/127.0.0.1 '$TEMP_HOSTNAME'/#127.0.0.1 '$TEMP_HOSTNAME'/' /etc/hosts
-sed -i 's/::1 '$TEMP_HOSTNAME'/#::1 '$TEMP_HOSTNAME'/' /etc/hosts
+sed -i 's/::1       '$TEMP_HOSTNAME'/#::1 '$TEMP_HOSTNAME'/' /etc/hosts
+
+systemctl stop firewalld
+systemctl disable firewalld
 ###############################################################################
 
 export NIC=eth1
