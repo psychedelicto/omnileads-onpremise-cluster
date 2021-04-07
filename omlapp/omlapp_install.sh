faccf32541fef76faa50a4afebad7dfda6a6a4ac
psychedelicto <>
2021-04-07 15:32:14 -0300

    test master

--- omlapp/omlapp_install.sh
+++ omlapp/omlapp_install.sh
@@ -52,16 +52,16 @@ cd ominicontacto && git checkout $omnileads_release
 git submodule init
 git submodule update
 
-##############################################
-cd modules/kamailio && git checkout develop
-cd ../asterisk && git checkout develop
-cd ../rtpengine && git checkout develop
-cd ../nginx && git checkout develop
-cd ../redis && git checkout develop
-cd ../postgresql && git checkout develop
-cd ../websockets && git checkout develop
-cd ../..
-##############################################
+# ##############################################
+# cd modules/kamailio && git checkout develop
+# cd ../asterisk && git checkout develop
+# cd ../rtpengine && git checkout develop
+# cd ../nginx && git checkout develop
+# cd ../redis && git checkout develop
+# cd ../postgresql && git checkout develop
+# cd ../websockets && git checkout develop
+# cd ../..
+# ##############################################
 
 echo "***************************** inventory setting *************************************"
 echo "***************************** inventory setting *************************************"
