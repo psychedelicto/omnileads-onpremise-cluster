29caa0909b15387855a924196f18eb5ec01b6fa1
psychedelicto <>
2021-04-08 10:12:03 -0300

    borro repos al final

--- 2_rtpengine/rtpengine_install.sh
+++ 2_rtpengine/rtpengine_install.sh
@@ -2,7 +2,7 @@
 
 echo "************************* yum update and install kernel-devel ***********************************"
 echo "************************* yum update and install kernel-devel ***********************************"
-yum update -y && yum install git python3-pip python3 -y
+yum update -y && yum install git python3-pip python3 kernel-devel -y
 pip3 install pip --upgrade
 pip3 install 'ansible==2.9.2'
 
@@ -25,7 +25,7 @@ git clone $COMPONENT_REPO
 cd omlrtpengine
 git checkout $COMPONENT_RELEASE
 cd deploy
-ansible-playbook rtpengine.yml -i inventory --extra-vars "iface=$NIC rtpengine_version=$(cat ../.rtpengine_version)"
+ansible-playbook rtpengine.yml -i inventory --extra-vars "rtpengine_version=$(cat ../.rtpengine_version)"
 
 echo "******************** Overwrite rtpengine.conf ***************************"
 echo "******************** Overwrite rtpengine.conf ***************************"
@@ -35,10 +35,9 @@ if [ "$SCENARIO" == "CLOUD" ]; then
 elif [ "$SCENARIO" == "LAN" ]; then
   echo "OPTIONS="-i $PRIVATE_IPV4  -o 60 -a 3600 -d 30 -s 120 -n $PRIVATE_IPV4:22222 -m 20000 -M 50000 -L 7 --log-facility=local1""  > /etc/rtpengine-config.conf
 elif [ "$SCENARIO" == "HYBRID_1_NIC" ]; then
-  echo "OPTIONS="-i external/$PRIVATE_IPV4!$PUBLIC_IPV4  -o 60 -a 3600 -d 30 -s 120 -n $PRIVATE_IPV4:22222 -m 20000 -M 50000 -L 7 --log-facility=local1""  > /etc/rtpengine-config.conf
+  echo "OPTIONS="-i internal/$PRIVATE_IPV4;external/$PUBLIC_IPV4  -o 60 -a 3600 -d 30 -s 120 -n $PRIVATE_IPV4:22222 -m 20000 -M 50000 -L 7 --log-facility=local1""  > /etc/rtpengine-config.conf
 elif [ "$SCENARIO" == "HYBRID_2_NIC" ]; then
-  echo "OPTIONS="-i external $PRIVATE_IPV4  -o 60 -a 3600 -d 30 -s 120 -n $PRIVATE_IPV4:22222 -m 20000 -M 50000 -L 7 --log-facility=local1""  > /etc/rtpengine-config.conf
-  echo "OPTIONS="-i external $PUBLIC_IPV4  -o 60 -a 3600 -d 30 -s 120 -n $PRIVATE_IPV4:22222 -m 20000 -M 50000 -L 7 --log-facility=local1""  > /etc/rtpengine-config.conf
+  echo "OPTIONS="-i $PRIVATE_IPV4  -o 60 -a 3600 -d 30 -s 120 -n $PRIVATE_IPV4:22222 -m 20000 -M 50000 -L 7 --log-facility=local1""  > /etc/rtpengine-config.conf
 else
   echo "ERROR you must to define the SCENARIO correctly"
 fi
