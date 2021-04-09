3d42d69f39c030b23fc7c9a5b0b4ac76cb6f6b24
psychedelicto <>
2021-04-09 10:47:52 -0300

    maria lan addr

--- 7_mariadb/user_data.sh
+++ 7_mariadb/user_data.sh
@@ -3,8 +3,8 @@
 REPO_URL=https://github.com/psychedelicto/omnileads-onpremise-cluster.git
 REPO_RELEASE=onpre-001-oml-2-punto-0
 
-export NIC=enp0s3
-export PRIVATE_IPV4=$(ip addr show $PRIVATE_NIC | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+NIC=enp0s3
+
 export DIALER_USER=wombat
 export DIALER_PASS=C11H15NO2
 
@@ -15,7 +15,7 @@ IPADDR_MASK=$(ip addr show $NIC | grep "inet\b" | awk '{print $2}')
 NETADDR_IPV4=$(ipcalc -n $IPADDR_MASK |cut -d = -f 2)
 NETMASK_PREFIX=$(ipcalc -m $IPADDR_MASK |cut -d= -f2)
 
-export LAN_ADDRESS="$NETADDR_IPV4\/$NETMASK_PREFIX"
+export LAN_ADDRESS="$NETADDR_IPV4/$NETMASK_PREFIX"
 
 cd $SRC
 git clone $REPO_URL
