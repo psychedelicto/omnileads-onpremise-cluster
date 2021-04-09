6d0b8343bc80b6e58f0618a30a9741849d4373d6
psychedelicto <>
2021-04-08 23:45:22 -0300

    postgres

--- 3_postgres/user_data.sh
+++ 3_postgres/user_data.sh
@@ -3,21 +3,23 @@
 REPO_URL=https://github.com/psychedelicto/omnileads-onpremise-cluster.git
 REPO_RELEASE=onpre-001-oml-2-punto-0
 
-export COMPONENT_REPO=https://gitlab.com/omnileads/omlrtpengine.git
+export COMPONENT_REPO=https://gitlab.com/omnileads/omlpgsql.git
 export COMPONENT_RELEASE=develop
 
 export NIC=enp0s3
-export SRC=/usr/src
+export SOURCE_DIR=/usr/src
 export OML_DB_NAME=omnileads
 export OML_USERNAME=omnileads
 export OML_PASSWORD=my_very_strong_pass
 
-yum install epel-release git ipcalc -y
+yum install -y epel-release
+yum install -y git ipcalc
 
 export IPADDR_MASK=$(ip addr show $NIC | grep "inet\b" | awk '{print $2}')
 export NETADDR_IPV4=$(ipcalc -n $IPADDR_MASK |cut -d = -f 2)
 export NETMASK_PREFIX=$(ip addr show $NIC | grep "inet\b" | awk '{print $2}' | cut -d/ -f2)
 
+cd $SOURCE_DIR
 git clone $REPO_URL
 cd omnileads-onpremise-cluster
 git checkout $REPO_RELEASE
